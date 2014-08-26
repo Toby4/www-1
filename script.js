53bc825760a5b2e40340eb9b2ca7586cc3b75c79
Monty Anderson <montyanderson1@googlemail.com>
2014-08-26 21:41:50 +0100

    added a bio

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ $(document).ready(function() {
 
 	$("h1").fitText(1.2);
 	$("h2").fitText(2);
-	$("div").fitText(1.0);
 	$("h3").fitText(3.2);
+	$("div").fitText(1.0);
+
+	function resize() {
+		//
+	}
+
+	resize();
+
+	$(document).resize(resize);
 
 });
